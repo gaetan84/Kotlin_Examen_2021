abba11c6ce2af88446be47461498512ac050d733
Gaétan Vogel <52194897+gaetan84@users.noreply.github.com>
2021-01-22 15:41:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_examen/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        print ("test")
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
